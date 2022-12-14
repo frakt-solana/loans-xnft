547b4bc7a0d481b7c52276721fdb3a465afa396b
sablevsky <vladislav1145@gmail.com>
2022-12-14 10:44:19 +0100

    Move back Tim's routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import ReactXnft, { Stack } from 'react-xnft'
+import { Stack, View, Text } from 'react-xnft'
 
 import Home from './Home'
 import Suggestions from './Suggestions'
+import { Frakt } from './iconsNew/Frakt'
 
 export function App() {
   return (
     <Stack.Navigator
       initialRoute={{ name: 'home' }}
-      options={({ route }) => ({
-        title: route.name,
-      })}
+      // @ts-ignore
+      options={({ route }) => {
+        switch (route.name) {
+          case 'home':
+            return {
+              title: (
+                <View style={{ display: 'flex', justifyContent: 'center' }}>
+                  <Frakt />
+                </View>
+              ),
+            }
+          case 'suggestions':
+            return { title: <Text>Select option</Text> }
+
+          default:
+            throw new Error('unknown route')
+        }
+      }}
+      style={{}}
     >
-      <Stack.Screen name={'home'} component={Home} />
+      <Stack.Screen name={'home'} component={() => <Home />} />
       <Stack.Screen name={'suggestions'} component={Suggestions} />
     </Stack.Navigator>
   )
