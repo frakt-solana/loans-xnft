4f92eec2e74df7e79aab2a4a949ba9d77dbaa7dc
Vladislav Plaksa <ezekiel.9218@gmail.com>
2022-12-29 11:58:25 +0300

    add createLoansService from sdk

--- src/hooks/useLoansService.ts
+++ src/hooks/useLoansService.ts
@@ -1,12 +1,14 @@
 import { useMemo } from 'react'
+import { createLoansService } from '@frakt-protocol/frakt-sdk/lib/loans/loansService'
 
 import { LOANS_FEE_ADMIN_PUBKEY, LOANS_PROGRAM_PUBKEY } from '../constants'
-import { createLoansService } from '../loansService'
+import { DEFAULT_BACKEND_DOMAIN } from './../loansService/constants'
 
 export const useLoansService = () => {
   return useMemo(
     () =>
       createLoansService({
+        apiDomain: DEFAULT_BACKEND_DOMAIN,
         programPublicKey: LOANS_PROGRAM_PUBKEY,
         adminPublicKey: LOANS_FEE_ADMIN_PUBKEY,
       }),
--- src/loansService/index.ts
+++ src/loansService/index.ts
@@ -1,2 +1,1 @@
-export { createLoansService } from './createLoansService'
 export * from './types'
